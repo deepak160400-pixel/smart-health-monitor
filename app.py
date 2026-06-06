980836e315d4fa62f4fc10d0e5d86cefcf8f60af
deepak160400-pixel <deepak160400@gmail.com>
2026-06-06 20:28:24 +0530

    Add files via upload
    Initial Smart Health Monitoring System

---
+++ app.py
@@ -0,0 +1,210 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import session
+
+import sqlite3
+
+
+app=Flask(__name__)
+
+app.secret_key="smart_health"
+
+
+def init_db():
+
+    con=sqlite3.connect(
+        "database.db"
+    )
+
+    cur=con.cursor()
+
+    cur.execute("""
+
+    CREATE TABLE IF NOT EXISTS users(
+
+    id INTEGER PRIMARY KEY,
+
+    email TEXT,
+
+    phone TEXT,
+
+    password TEXT
+
+    )
+
+    """)
+
+    con.commit()
+
+    con.close()
+
+
+init_db()
+
+
+@app.route("/")
+
+def login():
+
+    return render_template(
+        "login.html"
+    )
+
+
+@app.route(
+"/register",
+methods=["POST"]
+)
+
+def register():
+
+    email=request.form["email"]
+
+    phone=request.form["phone"]
+
+    password=request.form["password"]
+
+    con=sqlite3.connect(
+        "database.db"
+    )
+
+    cur=con.cursor()
+
+    cur.execute(
+
+    """
+
+    INSERT INTO users
+
+    (
+    email,
+    phone,
+    password
+
+    )
+
+    VALUES
+
+    (?,?,?)
+
+    """,
+
+    (
+    email,
+    phone,
+    password
+    )
+
+    )
+
+    con.commit()
+
+    con.close()
+
+    return render_template(
+
+    "login.html",
+
+    success=
+
+    "Registration Successful"
+
+    )
+
+
+@app.route(
+"/login",
+methods=["POST"]
+)
+
+def authenticate():
+
+    email=request.form["email"]
+
+    password=request.form["password"]
+
+    con=sqlite3.connect(
+        "database.db"
+    )
+
+    cur=con.cursor()
+
+    cur.execute(
+
+    """
+
+    SELECT *
+
+    FROM users
+
+    WHERE
+
+    email=?
+
+    AND
+
+    password=?
+
+    """,
+
+    (
+    email,
+    password
+    )
+
+    )
+
+    user=cur.fetchone()
+
+    con.close()
+
+    if user:
+
+        session["user"]=email
+
+        return redirect(
+            "/dashboard"
+        )
+
+    return render_template(
+
+    "login.html",
+
+    error=
+
+    "Invalid Credentials"
+
+    )
+
+
+@app.route(
+"/dashboard"
+)
+
+def dashboard():
+
+    if "user" not in session:
+
+        return redirect("/")
+
+    return render_template(
+        "dashboard.html"
+    )
+
+
+@app.route("/logout")
+
+def logout():
+
+    session.clear()
+
+    return redirect("/")
+
+
+if __name__=="__main__":
+
+    app.run(
+        debug=True
+    )
